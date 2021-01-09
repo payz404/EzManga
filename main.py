4e6eaf1bf26a39325de57b6faece5dc0b43c98e6
payz404 <46267341+payz404@users.noreply.github.com>
2021-01-10 06:06:54 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from ezmanga import manga as my
+from ezmanga import menu
+from ezmanga import *
+
+
+if __name__ == '__main__':
+   menu = menu.Menu()
+else:
+   print("Instaling Requirements...")
+   os.system("python3 -m pip install -r requirements.txt")
