9b9ee96291362cba3af175921563c0373e878759
payz404 <46267341+payz404@users.noreply.github.com>
2021-01-22 00:39:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from ezmanga import manga as my
 from ezmanga import menu
 from ezmanga import *
 
-
-if __name__ == '__main__':
-   menu = menu.Menu()
-else:
-   print("Instaling Requirements...")
-   os.system("python3 -m pip install -r requirements.txt")
+try:
+   
+   if __name__ == '__main__':
+      menu = menu.Menu()
+except:
+      os.system("python3 -m pip install -r requirements.txt")
